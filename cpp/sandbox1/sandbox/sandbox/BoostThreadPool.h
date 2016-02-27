6951446e1c510a0bd4e667cb3090cb71abee12f3
drewqb <drew@acm.org>
2016-02-27 12:47:20 -0500

    tweaks

--- cpp/sandbox1/sandbox/sandbox/BoostThreadPool.h
+++ cpp/sandbox1/sandbox/sandbox/BoostThreadPool.h
@@ -3,27 +3,48 @@
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
 
-struct BoostThreadPool {
+struct BoostThreadPool 
+{
 	typedef std::unique_ptr<boost::asio::io_service::work> asio_worker;
 
-	BoostThreadPool(int threads) : m_service(), m_service_worker(new asio_worker::element_type(m_service))
+	BoostThreadPool(int threads) : 
+		m_service(), 
+		m_service_worker(new asio_worker::element_type(m_service))
 	{
-		for (int i = 0; i < threads; ++i) {
+		m_service.stop();
+		for (int i = 0; i < threads; ++i) 
+		{
 			auto worker = [this] { return m_service.run(); };
 			m_ThreadGroup.add_thread(new boost::thread(worker));
 		}
+		/*for (int i = 0; i < threads; ++i)
+		{
+			m_ThreadGroup.create_thread(boost::bind(&boost::asio::io_service::run, &m_service));
+		}
+		*/
 	}
-
+	~BoostThreadPool() 
+	{
+		
+	}
+	void abort()
+	{
+		m_service.stop();
+		m_ThreadGroup.interrupt_all();
+	
+	}
+	void runAll()
+	{
+		m_service_worker.reset();
+		m_ThreadGroup.join_all();
+		m_service.stop();
+	}
 	template<class F>  void addTask(F f)
 	{
 		m_service.post(f);
 	}
 
-	~BoostThreadPool() {
-		m_service_worker.reset();
-		m_ThreadGroup.join_all();
-		m_service.stop();
-	}
+	
 
 private:
 	boost::asio::io_service m_service;
--- cpp/sandbox1/sandbox/sandbox/sandbox.cpp
+++ cpp/sandbox1/sandbox/sandbox/sandbox.cpp
@@ -15,7 +15,8 @@ public:
 		for (int x = 0; x < 5;++x)
 		{
 			printf("fun1()\n");
-			Sleep(100);
+			boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+			boost::thread_interrupted();
 		}
 	}
 	void fun2(int val)
@@ -23,13 +24,18 @@ public:
 		for (int x = 0; x < 10;++x)
 		{
 			printf("fun1(%d)\n", val);
-			Sleep(100);
+			boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+			boost::thread_interrupted();
 		}
 	}
 
 };
 
-
+void callAbort(BoostThreadPool *tp)
+{
+	tp->abort();
+	printf("abort threadpool\n");
+}
 int main()
 {
 	BoostThreadPool tp(10);
@@ -45,10 +51,15 @@ int main()
 		}
 		else
 		{
-			tp.addTask(boost::bind(&HandleIt::fun2, sp,10));
+			tp.addTask(boost::bind(&HandleIt::fun2, sp,x));
+		}
+		if (x == 3)
+		{
+			//tp.addTask(boost::bind(&callAbort, &tp));
 		}
 	}
-	
+	tp.runAll();
+
 	printf("hello");
     return 0;
 }
